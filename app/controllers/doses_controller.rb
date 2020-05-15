39771a6c0c349a6e7cf207ccb200e9901170a60c
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-05-15 18:27:07 +0200

    Did some front modifications on show page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,15 +1,16 @@
 class DosesController < ApplicationController
 
-  def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     @dose.save
+    redirect_to cocktail_path(@cocktail)
   end
 
   def destroy
